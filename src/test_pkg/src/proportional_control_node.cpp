4cc80bb27dfce0aa6f2bd975d79f6348acf40401
sej0015 <60272968+sej0015@users.noreply.github.com>
2021-07-18 10:11:55 -0400

    Updated proportional control node to include heading control

--- src/test_pkg/src/proportional_control_node.cpp
+++ src/test_pkg/src/proportional_control_node.cpp
@@ -1,10 +1,7 @@
-// Proportional controller
+// Proportional controller for waypoint following and heading corrections
 
-
-// This node was made to try and turn all three wheels from teleop_twist_joy
 
 // Things to include to make it work
-// TODO: Include a different type of message
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <math.h>
@@ -12,46 +9,53 @@
 #include <string>
 #include <geometry_msgs/TransformStamped.h>
 
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Quaternion.h"
+#include "tf/transform_datatypes.h"
+#include "tf/LinearMath/Matrix3x3.h"
+
 //Declare some variables
 
-  std::string waypoint_topic_name = "waypoint_topic"; // TODO: Change this later to waypoint topic name
-  std::string vicon_topic_name = "vicon/turtle3/turtle3"; //TODO: Change this later
-  std::string cmd_vel_topic_name = "cmd_vel";
-  int loop_rate = 10;
-  double waypoint_x = 0; // waypoints that this node recieves. The goal locations. 
-  double waypoint_y = 0;
-  double true_x = 0; // current pose according to the vicon sensors
-  double true_y = 0;
-	double max_velocity = 0.5; // max velocity of 0.5 units/s
-  
+std::string waypoint_topic_name = "waypoint_topic"; // TODO: Change this later to use the config file
+std::string vicon_topic_name = "vicon/turtle3/turtle3"; //TODO: Change this later to use the config file
+std::string cmd_vel_topic_name = "cmd_vel";
+int loop_rate = 10;
+double waypoint_x = 0; // waypoints that this node recieves. The goal locations. 
+double waypoint_y = 0;
+double true_x = 0; // current pose according to the vicon sensors
+double true_y = 0;
+double true_yaw = 0;
+double max_velocity = 0.5; // max velocity of 0.5 units/s
+
 //Declare callback functions.
-  void waypoint_topic_Callback(const geometry_msgs::Twist::ConstPtr& msg); //TODO: Change type
-  void vicon_topic_Callback(const geometry_msgs::TransformStamped::ConstPtr& msg); //TODO: Change Type
-  
+void waypoint_topic_Callback(const geometry_msgs::Twist::ConstPtr& msg);
+void vicon_topic_Callback(const geometry_msgs::TransformStamped::ConstPtr& msg);
+
 
 //Start Main
 int main(int argc, char** argv)
 {
 
-  //Initialize Ros
-  std::string node_name = "proportional_control_node";
-  ros::init(argc, argv, node_name);
-  ros::NodeHandle nh;
+	//Initialize Ros
+	std::string node_name = "proportional_control_node";
+	ros::init(argc, argv, node_name);
+	ros::NodeHandle nh;
 
-  // Initialize publishers and subscribers
-  
-ros::Subscriber waypoint_subscriber = nh.subscribe(waypoint_topic_name, 1, waypoint_topic_Callback);
+	// Initialize publishers and subscribers
 
-ros::Subscriber vicon_subscriber = nh.subscribe(vicon_topic_name, 1, vicon_topic_Callback);
+	ros::Subscriber waypoint_subscriber = nh.subscribe(waypoint_topic_name, 1, waypoint_topic_Callback);
 
-ros::Publisher cmd_vel_publisher = nh.advertise<geometry_msgs::Twist>(cmd_vel_topic_name,1);
+	ros::Subscriber vicon_subscriber = nh.subscribe(vicon_topic_name, 1, vicon_topic_Callback);
+
+	ros::Publisher cmd_vel_publisher = nh.advertise<geometry_msgs::Twist>(cmd_vel_topic_name, 1);
 
 
-  ros::Rate rate(loop_rate);
+	ros::Rate rate(loop_rate);
 
-  while(ros::ok())
-  {
-   // Math stuff happens below. The actual P controller is at work below. 
+	while (ros::ok())
+	{
+		// Math stuff happens below. The actual P controller is at work below. 
+		 // this is the one for the waypoint following
 
 		double k = 0.3;
 
@@ -61,36 +65,50 @@ ros::Publisher cmd_vel_publisher = nh.advertise<geometry_msgs::Twist>(cmd_vel_to
 
 		double error_x = waypoint_x - true_x; // calculated error x component
 		double error_y = waypoint_y - true_y; // caclulated error y component
-		
-		double norm = sqrt(error_x*error_x + error_y * error_y); // magnitude of the 
-		double unit_x = error_x/norm; // x component of unit vector
-		double unit_y = error_y/norm; // y component of unit vector
-		
-		if(norm > 0.05){
+
+		double norm = sqrt(error_x * error_x + error_y * error_y); // magnitude of the 
+		double unit_x = error_x / norm; // x component of unit vector
+		double unit_y = error_y / norm; // y component of unit vector
+
+		if (norm > 0.05) {
 
 			// proportional velocity vector
 			vel_x = k * error_x;
 			vel_y = k * error_y;
 
 			// check if the computed velocity is larger than the max.
-			double check_vel = sqrt(vel_x*vel_x + vel_y* vel_y);
-			
+			double check_vel = sqrt(vel_x * vel_x + vel_y * vel_y);
+
 			// if it is larger, multipl the unit vector of the error by the max velocity
-			if(check_vel > max_velocity){
+			if (check_vel > max_velocity) {
 				vel_x = unit_x * max_velocity;
 				vel_y = unit_y * max_velocity; // the resultant vector is at the maximum speed in the direction. 
 			}
 
 		}
-		else{
+		else {
 			vel_x = 0;
-			vel_y = 0; 
+			vel_y = 0;
 
 		}
-	// storing the x and y velocity to be published
-	output_vel.linear.x = vel_x;
-	output_vel.linear.y = vel_y; 
-	
+		// storing the x and y velocity to be published
+		output_vel.linear.x = vel_x;
+		output_vel.linear.y = vel_y;
+
+		//Now For the p-controller for heading correction
+		double heading_k = 2;
+		double heading_error = true_yaw;
+		output_vel.angular.z = heading_k * heading_error;
+		if (output_vel.angular.z > max_velocity) {
+			if (output_vel.angular.z > 0) {
+				output_vel.angular.z = max_velocity;
+			}
+			else {
+				output_vel.angular.z = max_velocity * -1;
+			}
+		}
+
+	//Publish the command velocity to the robot
 	cmd_vel_publisher.publish(output_vel); 
 
     ros::spinOnce();
@@ -99,73 +117,25 @@ ros::Publisher cmd_vel_publisher = nh.advertise<geometry_msgs::Twist>(cmd_vel_to
 }
 
 
-void waypoint_topic_Callback(const geometry_msgs::Twist::ConstPtr& msg) //TODO: MEssage type needs to be set <<
+void waypoint_topic_Callback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-
 waypoint_x = msg -> linear.x;
 waypoint_y = msg -> linear.y; 
-
-
-
-/* NOTE: old code below from the holonomic controller. Used as reference for currnet proportional controller code. 
-  cmd_vel_x = msg -> linear.x*-2;
-  cmd_vel_y = msg -> linear.y*2;
-  cmd_omega = msg -> angular.z*-2;
-  double norm = sqrt(cmd_vel_x*cmd_vel_x + cmd_vel_y*cmd_vel_y);
-  double unit_x = cmd_vel_x /(norm);
-  double unit_y = cmd_vel_y /(norm);
-  if((sqrt(cmd_vel_x*cmd_vel_x + cmd_vel_y*cmd_vel_y)) == 0) {
-    unit_x = 0;
-    unit_y = 0;
-  }
-  
-  right_vel_out = round((0.58*unit_x*norm -0.33*unit_y*norm + 0.33*cmd_omega*2)*maxVel); 
-  ROS_INFO("%i", right_vel_out);
-  left_vel_out = round((-0.58*unit_x*norm -0.33*unit_y*norm + 0.33*cmd_omega*2)*maxVel);
-  back_vel_out = round((0*unit_x*norm + 0.67*unit_y*norm + 0.33*cmd_omega*2)*maxVel);
-  //wheel_vel_out = round(cmd_vel/cmd_vel_max * maxVel);
-*/
-
 }
 
-void vicon_topic_Callback(const geometry_msgs::TransformStamped::ConstPtr& msg){ //TODO: MEssage type needs to be set <<
-	
+void vicon_topic_Callback(const geometry_msgs::TransformStamped::ConstPtr& msg){
 	true_x = msg -> transform.translation.x; //obtainin the data from the vicon sensor about the pose.
 	true_y = msg -> transform.translation.y;
 
+	// the incoming geometry_msgs::Transformed is transformed to a tf::Quaterion
+	tf::Quaternion quat;
+	tf::quaternionMsgToTF(msg->transform.quaternion, quat);
 
-
-
-
+	// the tf::Quaternion has a method to acess roll pitch and yaw
+	double roll, pitch, yaw;
+	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+	true_yaw = yaw;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
